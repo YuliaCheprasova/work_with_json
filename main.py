328012de6cdd45f3beb571ce6b05495290675ebf
YuliaCheprasova <cheprasovaja@yandex.ru>
2023-04-29 18:23:23 +0700

    ready ndjson1

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import json
 import psycopg2
+import re
 
-with open("bigproba.json", "r") as f:
+"""with open("bigproba.json", "r") as f:
     line=f.readline()
     line = line.replace("{\"result\": {", "[")
     line = line.replace("\"event\": ", "")
     line = line.replace("}}}", "}]")
-    print(line)
-    with open ('hope.json', 'w') as f1:
-        f1.write(line)
+    line = line.replace("\\u", "")
+    line = line.replace("\\/", "")
+    line = re.sub()
 
-with open('hope.json', 'r') as f2:
+    with open ('hope.json', 'w') as f1:
+        f1.write(line)"""
+
+"""with open ('hope.json', 'r') as f4:
+    line = f4.readline()
+    line = line.replace("\\u", "")
+    line = line.replace("\\/", "")
+    with open('hope3.json', 'w') as f5:
+        f5.write(line)"""
+
+"""with open ('hope2.json', 'r') as f4:
+    line = f4.readline()
+    line = line.replace("\"\"", "")
+    with open('hope4.json', 'w') as f5:
+        f5.write(line)"""
+
+with open('hope4.json', 'r') as f2:
     data = json.load(f2)
     ndJson = ''
     for j in data:
         ndJson += json.dumps(j, ensure_ascii=False) + '\n'
-with open('ndjson1.json','w') as f3:
+with open('ndjson1.json', 'w') as f3:
     f3.write(ndJson)
-print(ndJson)
+
